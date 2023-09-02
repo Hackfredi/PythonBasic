2203d4a2344f7ca5fe5bed694200b3de98254ffc
fredi4302 <devangbisht43@gmail.com>
2023-09-02 23:55:17 +0530

    Update String Files.py

--- String Files.py
+++ String Files.py
@@ -13,3 +13,7 @@ print(phase.index("l"))
 
 #to find the sequence starting
 print(phase.index("el"))
+
+#if you want to add number to a string directly you will get an error you have to convert int data type to string
+d1=500
+print(str(d1)+"gho gho")
