d926b38722e652c22fc6f96230abb2e03df4c38a
fredi4302 <fredigaming4302@gmail.com>
2023-09-03 09:02:28 +0530

    Update data_type.py

--- data_type.py
+++ data_type.py
@@ -1,3 +1,28 @@
+#to import all method from math class
+from math import *
+
 #if you want to add number to a string directly you will get an error you have to convert int data type to string
 d1=500
 print(str(d1)+"gho gho")
+
+#calculator
+
+#addition
+a1=input("Enter first number")
+a2=input("Enter second number")
+print(int(a1)+int(a2))
+
+#subtraction
+s1=input("Enter first number")
+s2=input("Enter second number")
+print(int(s1)-int(s2))
+
+#multiplication
+m1=input("Enter first number")
+m2=input("Enter second number")
+print(int(m1)*int(m2))
+
+#division
+d1=input("Enter first number")
+d2=input("Enter second number")
+print(float(d1)/float(d2))
