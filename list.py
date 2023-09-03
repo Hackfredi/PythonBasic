87f10429c3869db60577ce79252bd8380ca2d2ea
fredi4302 <fredigaming4302@gmail.com>
2023-09-03 19:26:04 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,3 +1,24 @@
 #basic list
 ff=[1,2,3,4,5]
 print(ff)
+
+ff.append(52)
+print(ff)
+
+ff.insert(1,52)
+print(ff)
+
+ff.remove(52)
+print(ff)
+
+#Clear your list ff.clear()
+print(ff)
+
+ff.pop()
+print(ff)
+
+print(ff.index(3))
+
+kj=[5,20,9721,2,31,0]
+kj.sort()
+print(kj)
